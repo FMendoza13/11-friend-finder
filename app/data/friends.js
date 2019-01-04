4fc205d97874866f84a69b50ad45f5bf2e02d427
Fernando Mendoza <fern.mendoza13@gmail.com>
2019-01-03 23:05:23 -0800

    changed stock pictures to the pictures of super friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
-		name: "Eitan",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		name: "Superman",
+		photo: "http://dc.wikia.com/wiki/Superman_(Clark_Kent)?file=Superman_0002.jpg",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
-		name: "Juliette",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		name: "Wonder Woman",
+		photo: "http://dc.wikia.com/wiki/Diana_of_Themyscira_(Prime_Earth)?file=Wonder_Woman_Vol_5_4_Textless.jpg",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
-		name: "Kevin",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		name: "Batman",
+		photo: "http://dc.wikia.com/wiki/Bruce_Wayne_(Prime_Earth)?file=Batman_Vol_2_2_Variant_Textless.jpg",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
-		name: "Amiah",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		name: "Robin",
+		photo: "http://dc.wikia.com/wiki/Damian_Wayne_(Prime_Earth)?file=Teen_Titans_Rebirth_Vol_1_1_Textless.jpg",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
-		name: "Tyler",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		name: "Aquaman",
+		photo: "http://dc.wikia.com/wiki/Arthur_Curry_(Prime_Earth)?file=Aquaman_0176.jpg",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
